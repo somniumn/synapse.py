bd62b94fce2881ad117adb88076daf1f1cb7e38c
Jeongseob Kim <40890895+somniumn@users.noreply.github.com>
2020-12-04 00:45:02 +0900

    update visualize

---
+++ visualize/plot_gaussian_mixture.py
@@ -0,0 +1,64 @@
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.mixture import GaussianMixture
+from scipy.spatial.distance import cdist
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.patches import Ellipse
+import seaborn as sns
+
+font_cs = {'fontname': 'Consolas'}
+
+# Generate some data
+X, y_true = make_blobs(n_samples=400, centers=4, cluster_std=0.60, random_state=0)
+X = X[:, ::-1]  # flip axes for better plotting
+
+gmm = GaussianMixture(n_components=4, covariance_type='full').fit(X)
+
+labels = gmm.predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis')
+
+probs = gmm.predict_proba(X)
+print(probs[:5].round(3))
+
+size = 50 * probs.max(1) ** 2  # square emphasizes differences
+plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', s=size)
+
+
+def draw_ellipse(position, covariance, ax=None, **kwargs):
+    """Draw an ellipse with a given position and covariance"""
+    ax = ax or plt.gca()
+
+    # Convert covariance to principal axes
+    if covariance.shape == (2, 2):
+        U, s, Vt = np.linalg.svd(covariance)
+        angle = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
+        width, height = 2 * np.sqrt(s)
+    else:
+        angle = 0
+        width, height = 2 * np.sqrt(covariance)
+
+    # Draw the Ellipse
+    for nsig in range(1, 4):
+        ax.add_patch(Ellipse(position, nsig * width, nsig * height,
+                             angle, **kwargs))
+
+
+def plot_gmm(gmm, X, label=True, ax=None):
+    ax = ax or plt.gca()
+    labels = gmm.fit(X).predict(X)
+    if label:
+        ax.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis', zorder=2)
+    else:
+        ax.scatter(X[:, 0], X[:, 1], s=40, zorder=2)
+    ax.axis('equal')
+
+    w_factor = 0.2 / gmm.weights_.max()
+    for pos, cov, w in zip(gmm.means_, gmm.covariances_, gmm.weights_):
+        draw_ellipse(pos, cov, alpha=w * w_factor)
+
+
+# sns.set_theme(style='whitegrid')
+plot_gmm(gmm, X)
+plt.grid()
+plt.show()
